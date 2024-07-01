e41b4a04c8398192c0966f6f67cb029a4ec1fb97
Zecyel <zecyel@163.com>
2024-07-01 11:15:58 +0800

    upd: typescript/

---
+++ typescript/assets/slidev/default-D9DR_qCM.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-Dm_EK0Sf.js";import{_ as r}from"../index-TbvLzh3t.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
