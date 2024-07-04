b7a2a542292c58959132a9915e3a5c17e45c20d4
Zecyel <zecyel@163.com>
2024-07-04 17:05:42 +0800

    fix: rename active forwarding to programmatic routing

---
+++ multipage/assets/slidev/default-DpeXFMIK.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-BVG37Fmo.js";import{_ as r}from"../index-CKE5-2AU.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
