1b640914fb465149075b0165fb94f59cab72f0e8
Zecyel <zecyel@163.com>
2024-07-13 20:14:46 +0800

    doc: add database

---
+++ database/assets/slidev/default-H4q8svhB.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-CdAPATPx.js";import{_ as r}from"../index-BSXhj0GC.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
